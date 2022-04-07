8f74f7b6f18fc5febb0f95d36c5a8cacb24015ce
Axel Ingelsson Fredler <axelingelsson@hotmail.com>
2022-04-07 16:59:14 +0200

    PlayerController experiments
    Started experimenting with a simple cube and the Unity's input system.

---
+++ SPM-Grupparbete Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
+
+public class PlayerController : MonoBehaviour
+{
+    private PlayerInput _PlayerInput;
+    private InputAction _MoveAction;
+    private InputAction _JumpAction;
+    private Vector3 _Velocity;
+    [FormerlySerializedAs("_Acceleration")] [SerializeField] [Range(1.0f, 50.0f)] private float _MovementAcceleration = 5.0f;
+    [SerializeField] [Range(1.0f, 10.0f)] private float _JumpForce = 2.0f;
+
+    private void Awake()
+    {
+        _PlayerInput = GetComponent<PlayerInput>();
+        _MoveAction = _PlayerInput.actions["Move"];
+        _JumpAction = _PlayerInput.actions["Jump"];
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Velocity = _MoveAction.ReadValue<Vector2>() * _MovementAcceleration;
+        transform.position += new Vector3(_Velocity.x, 0.0f, _Velocity.y) * Time.deltaTime;
+        if (_JumpAction.triggered)
+        {
+            transform.position += new Vector3(0.0f, _JumpForce, 0.0f);
+        }
+    }
+}
