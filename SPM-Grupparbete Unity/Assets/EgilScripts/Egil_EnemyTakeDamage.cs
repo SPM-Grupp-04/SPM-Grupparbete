c028ec36dac27108d53bd5fa79d1111690fdcd39
Egil-boop <82576944+Egil-boop@users.noreply.github.com>
2022-04-17 08:45:58 +0200

    EnemyTakeDamage

---
+++ SPM-Grupparbete Unity/Assets/EgilScripts/Egil_EnemyTakeDamage.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+
+public class Egil_EnemyTakeDamage : MonoBehaviour
+{
+    public bool getIsDead()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void killEnemy()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void TakeDamage()
+    {
+        throw new System.NotImplementedException();
+    }
+}
