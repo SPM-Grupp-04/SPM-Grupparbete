65a2b7e0f77f5dee90c6540c0bbfd3513ce62a2f
Axel Ingelsson Fredler <axelingelsson@hotmail.com>
2022-05-09 17:42:13 +0200

    Updated dynamite mechanic
    - When the dynamite explodes, the camera will shake and rocks will start falling down in the area near the explosion for a set duration.

---
+++ SPM-Grupparbete Unity/Assets/Scripts/CameraScripts/CameraShake.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+using Cinemachine;
+
+public class CameraShake : MonoBehaviour
+{
+    private static CameraShake instance;
+    
+    public static CameraShake Instance { 
+        get 
+        {
+            return instance;
+        }
+    }
+
+    [SerializeField] private CinemachineVirtualCamera cmVirtualCamera;
+
+    private float cameraShakeTime;
+    private float totalCameraShakeTime;
+
+    private float startMagnitude;
+
+    private CinemachineBasicMultiChannelPerlin cmBasicMultiChannelPerlin;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+        cmBasicMultiChannelPerlin =
+            cmVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+    }
+
+    public void ShakeCamera(float magnitude, float duration)
+    {
+        cmBasicMultiChannelPerlin.m_AmplitudeGain = magnitude;
+
+        startMagnitude = magnitude;
+        totalCameraShakeTime = duration;
+        cameraShakeTime = duration;
+    }
+
+    private void Update()
+    {
+        if (cmVirtualCamera != null)
+        {
+            if (cameraShakeTime > 0.0f)
+            {
+                cameraShakeTime -= Time.deltaTime;
+                if (cameraShakeTime <= 0.0f)
+                {
+                    cmBasicMultiChannelPerlin.m_AmplitudeGain = 
+                        Mathf.Lerp(startMagnitude, 0.0f, 1.0f - (cameraShakeTime / totalCameraShakeTime));
+                }
+            }
+        }
+    }
+}
--- SPM-Grupparbete Unity/Assets/Scripts/Environmental Hazards/FallingRocksTrapScript.cs
+++ SPM-Grupparbete Unity/Assets/Scripts/Environmental Hazards/FallingRocksTrapScript.cs
@@ -1,13 +1,52 @@
 //Author: Simon Canb�ck, sica 4801
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 
 public class FallingRocksTrapScript : MonoBehaviour
 {
     [SerializeField, Range(0.1f, 1.0f), Tooltip("Percentage chance of spawning per second.")] private float spawnThreshold = 0.5f;
     [SerializeField] private Vector3 rockStartingForce;
+
+    [SerializeField] private float rockSpawnDuration = 3.0f;
+
+    [SerializeField] [Range(1.0f, 20.0f)] private float cameraShakeMagnitude;
+    [SerializeField] [Range(0.1f, 5.0f)] private float cameraShakeDuration;
+
+    private float rockSpawnCountDown;
+    
+    [SerializeField] private GameObject rockPrefab;
+    private Bounds bounds;
+
+    [SerializeField] private Collider spawnArea;
+
+    private CameraShake cameraShake;
+
+    private Vector3 cameraFollowPoint;
+
+    private static FallingRocksTrapScript instance;
+    
+    public static FallingRocksTrapScript Instance
+    {
+        get { return instance; }
+    }
+
+    private bool spawnRocks;
+    
+
+    public bool SpawningRocks()
+    {
+        return spawnRocks;
+    }
+
+    public void SpawnRocks(bool spawnRocksState)
+    {
+        spawnRocks = spawnRocksState;
+    }
     private float SpawnThreshold
     {
         get
@@ -15,21 +54,52 @@ public class FallingRocksTrapScript : MonoBehaviour
             return Mathf.Pow(1.0f - spawnThreshold, Time.deltaTime); //chance per second
         }
     }
-    [SerializeField] private GameObject rockPrefab;
-    private Bounds bounds;
+    private void Awake()
+    {
+        cameraShake = CameraShake.Instance;
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        bounds = GetComponent<Collider>().bounds;
+        //bounds = GetComponent<Collider>().bounds;
        // print(SpawnThreshold);
         //print(Application.targetFrameRate);
+    }
+
+    public void SetFallingRockAreaPosition(Vector3 fallingRockAreaPosition)
+    {
+        transform.position = fallingRockAreaPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        SpawnRock();
+        if (spawnRocks)
+        {
+            StartCoroutine(SpawnRocksForDuration());
+            cameraShake.ShakeCamera(cameraShakeMagnitude, cameraShakeDuration);
+            spawnRocks = false;
+        }
+    }
+
+    private IEnumerator SpawnRocksForDuration()
+    {
+        rockSpawnCountDown = rockSpawnDuration;
+        do
+        {
+            SpawnRock();
+            rockSpawnCountDown -= Time.deltaTime;
+            yield return null;
+        } while (rockSpawnCountDown > 0.0f);
     }
 
     private void SpawnRock()
@@ -37,18 +107,18 @@ public class FallingRocksTrapScript : MonoBehaviour
         if (Random.value < SpawnThreshold)
             return;
 
-        float x = 0.0f;
-        float z = 0.0f;
+        float x = transform.position.x;
+        float z = transform.position.z;
 
         do
         {
-            x = (Random.value - 0.5f) * 2.0f * bounds.extents.x; //[0.0f, 1.0f] -> [-1.0f, 1.0f]
-            z = (Random.value - 0.5f) * 2.0f * bounds.extents.z;
-        } while (!(bounds.Contains(
-            new Vector3(bounds.center.x + x, bounds.center.y, bounds.center.z + z))) //reject coordinates outside bounds
+            x = (Random.value - 0.5f) * 2.0f * spawnArea.bounds.extents.x; //[0.0f, 1.0f] -> [-1.0f, 1.0f]
+            z = (Random.value - 0.5f) * 2.0f * spawnArea.bounds.extents.z;
+        } while (!(spawnArea.bounds.Contains(
+            new Vector3(spawnArea.bounds.center.x + x, spawnArea.bounds.center.y, spawnArea.bounds.center.z + z))) //reject coordinates outside bounds
             );
 
-        GameObject tempRock = Instantiate(rockPrefab, new Vector3(bounds.center.x + x, bounds.center.y, bounds.center.z + z), Quaternion.identity);
+        GameObject tempRock = Instantiate(rockPrefab, new Vector3(spawnArea.bounds.center.x + x, spawnArea.bounds.center.y, spawnArea.bounds.center.z + z), Quaternion.identity);
         tempRock.GetComponent<Rigidbody>().AddForce(rockStartingForce);
     }
 }
