122b8b9666ed7de2a4d59be649ac8f09bef97cad
emilkor <emil.kornung@gmail.com>
2022-05-20 09:56:48 +0200

    :)

--- SPM-Grupparbete Unity/Assets/Scripts/EnemyAI/BossAI.cs
+++ SPM-Grupparbete Unity/Assets/Scripts/EnemyAI/BossAI.cs
@@ -40,7 +40,7 @@ public class BossAI : Tree, IDamagable
 
     [SerializeField] private MeleeWepon meleeWepon;
 
-    private float currentHealth = 50;
+    [SerializeField] private float currentHealth = 50;
 
     protected override TreeNode SetUpTree()
     {
