dfa635a9842f93f17421009804db8e64f58737b7
Mr Neutron <axelingelsson@hotmail.com>
2022-05-29 17:22:19 +0200

    Updated dynamite
    - Player thrown dynamites now don't collide with shields, enemies, or the other player
    - Enemy thrown dynamites now don't cause camera shakes to happen

---
+++ SPM-Grupparbete Unity/Assets/ShieldCollisionDetection.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldCollisionDetection : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<PlayerController>().InsideShield = true;
+            Debug.Log("Inside Shield: " + other.gameObject.GetComponent<PlayerController>().InsideShield);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<PlayerController>().InsideShield = false;
+        }
+    }
+}
