303f5ed19c8a85c8c2903843e8ccf50a01e998d3
emilkor <emil.kornung@gmail.com>
2022-05-31 11:17:16 +0200

    das

--- SPM-Grupparbete Unity/Assets/Scripts/Pooling/EnemySpawner.cs
+++ SPM-Grupparbete Unity/Assets/Scripts/Pooling/EnemySpawner.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using EnemyAI;
 using EnemyAI.EnemyAIHandler;
+using Mono.Cecil.Cil;
 using Unity.Jobs.LowLevel.Unsafe;
 using Unity.Mathematics;
 using UnityEngine;
@@ -14,6 +15,7 @@ using Random = UnityEngine.Random;
 public class EnemySpawner : MonoBehaviour
 {
     private ObjectPool<BaseEnemyAI> pool;
+    
     private BaseEnemyAI enemy;
     private Vector3 SpawnPos;
     private BoxCollider boxCollider;
@@ -35,10 +37,10 @@ public class EnemySpawner : MonoBehaviour
 
     private void Awake()
     {
-        Debug.Log("Awake in EnemySpawner");
+        
         timer = totalAllowedSpawnTime;
         pool = new ObjectPool<BaseEnemyAI>(CreateEnemy, OnTakeEnemyAIFromPool, OnReturnBallToPool);
-        Debug.Log("After Creating a new pool");
+       
         enemyAIHandler = GetComponent<EnemyAIHandler>();
 
         // Räknar ut vad som är 100%
@@ -55,7 +57,7 @@ public class EnemySpawner : MonoBehaviour
 
         boxCollider = GetComponent<BoxCollider>();
         SpawnPos = Random.insideUnitSphere + (transform.position * boxCollider.size.x * boxCollider.size.z);
-        Debug.Log("After Calculating how many enemies that should spawn");
+       
         for (var i = 0; i < genericListOfBaseClassEnemyAI.Length; i++) // 2 gånger
         {
             enemy = genericListOfBaseClassEnemyAI[i];
@@ -64,28 +66,28 @@ public class EnemySpawner : MonoBehaviour
                 CreateEnemy();
             }
         }
-        Debug.Log("After creating enemies. End of Awake.");
+       
     }
 
     private void Start()
     {
-        Debug.Log("In start set all enemies to Inactive");
+        
         foreach (var enemyAI in enemyAIHandler.units)
         {
             enemyAI.gameObject.SetActive(false);
         }
-        Debug.Log("End of start.");
+      
     }
     
     private void OnEnable()
     {
-        Debug.Log("Entering Enable");
+      
         foreach (var enemyAI in enemyAIHandler.units)
         {
             enemyAI.gameObject.SetActive(true);
         }
         timer = totalAllowedSpawnTime;
-        Debug.Log("Exiting enable");
+    
     }
 
     
@@ -94,8 +96,8 @@ public class EnemySpawner : MonoBehaviour
         
         if (pool.CountActive < totalAllowedEnimesAtSpawner && timer > 0)
         {
-            Debug.Log("Setting spawn position ");
-            SpawnPos = Random.insideUnitSphere + (transform.position * boxCollider.size.x * boxCollider.size.z);
+           SpawnPos = Random.insideUnitSphere + (transform.position * boxCollider.size.x * boxCollider.size.z);
+           Debug.Log(pool.CountInactive + " Counting the pools inactive"); // 0 I byggdV
             for (var i = 0; i < pool.CountInactive; i++)
             {
                 Debug.Log("Removing objekts from the pool");
@@ -114,13 +116,13 @@ public class EnemySpawner : MonoBehaviour
     private BaseEnemyAI CreateEnemy()
     {
         // så att man kan sätta hur många % av en typ man vill ska finnas.
-
+        Debug.Log("CreateEnemy");
         enemy = Instantiate(enemy, transform.position, quaternion.identity);
         enemyAIHandler.units.Add(enemy);
 
         enemy.SetPool(pool);
         
-        Debug.Log("Should have spawned Enemies");
+        Debug.Log("End of createEnemy");
         return enemy;
     }
 
