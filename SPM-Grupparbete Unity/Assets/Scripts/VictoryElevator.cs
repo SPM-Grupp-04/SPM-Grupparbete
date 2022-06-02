3bb8447f5bb6196b1900275bcabb33724a263964
Egil-boop <82576944+Egil-boop@users.noreply.github.com>
2022-06-02 09:53:55 +0200

    Victory

---
+++ SPM-Grupparbete Unity/Assets/Scripts/VictoryElevator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utility
+{
+    public class VictoryElevator : MonoBehaviour
+    {
+        private int victorNumber;
+        private Animator animator;
+        private void Start()
+        {
+            victorNumber = GlobalControl.Instance.playerStatistics.componentsCollectedNumber;
+            animator = GetComponent<Animator>();
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            
+            Time.timeScale = 0;
+            animator.SetBool("doorOpen",victorNumber >= 5 );
+            
+        }
+
+        /*
+         * Den ska kolla om du har 5 komponenter kolla om spelaren går in i 
+            triggern spela animationen 
+         */
+    }
+}
