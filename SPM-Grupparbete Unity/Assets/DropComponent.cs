81c92d53542b679b1a8a371b22e5c74563727e73
emilkor <emil.kornung@gmail.com>
2022-06-03 10:55:26 +0200

    SistaPill

--- SPM-Grupparbete Unity/Assets/DropComponent.cs
+++ SPM-Grupparbete Unity/Assets/DropComponent.cs
@@ -7,21 +7,11 @@ using UnityEngine;
 public class DropComponent : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI text;
-    [SerializeField] private TextMeshProUGUI explainText;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         text.text = PlayerStatistics.Instance.componentsCollectedNumber + "/5";
-
-        if (PlayerStatistics.Instance.componentsCollectedNumber > 1)
-        {
-            explainText.enabled = false;
-        }
-        else
-        {
-            explainText.enabled = true;
-        }
     }
     
         
